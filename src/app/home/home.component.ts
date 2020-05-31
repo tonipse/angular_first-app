88fd53324573f11fc35465205aed1c3f23378807
Toni Peise <toni.peise@gmx.net>
2020-05-31 17:50:54 +0200

    move welcome

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TimelineMax, Power3 } from 'gsap';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   clickCounter: number = 0;
   name: string = '';
+  password: string = '';
+  tl = new TimelineMax();
+  px;
   constructor() {}
 
   ngOnInit(): void {}
 
   countClick() {
     this.clickCounter += 1;
+  }
+  comeBack(gs) {
+    this.tl.to(gs, 0.5, {
+      css: { transform: ` translateX(0px) ` },
+      ease: Power3.easeInOut,
+    });
+  }
+  gsinput(gs) {
+    this.tl.to(gs, 0.5, {
+      css: { transform: ` translateX(${this.px}px) ` },
+      ease: Power3.easeInOut,
+    });
   }
 }
